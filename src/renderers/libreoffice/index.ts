d86c069324651a7a5a628bea59dbd5489b6cf53b
Jamie Blair <info@orangemug.co.uk>
2024-11-15 09:26:39 +0000

    npm run lint:format

--- src/renderers/libreoffice/index.ts
+++ src/renderers/libreoffice/index.ts
@@ -71,14 +71,16 @@ export async function isSupported(format: OfficeOpenXmlType) {
   }
 
   if (platform === "win32") {
-    const rslt = await $`where C:\\Program Files (x86)\\LibreOffice 5\\program\\soffice.exe`;
-    return rslt.exitCode === 0
+    const rslt =
+      await $`where C:\\Program Files (x86)\\LibreOffice 5\\program\\soffice.exe`;
+    return rslt.exitCode === 0;
   } else if (platform === "darwin") {
-    const rslt = await $`command -v /Applications/LibreOffice.app/Contents/MacOS/soffice`;
-    return rslt.exitCode === 0
+    const rslt =
+      await $`command -v /Applications/LibreOffice.app/Contents/MacOS/soffice`;
+    return rslt.exitCode === 0;
   } else if (platform === "linux") {
     const rslt = await $`command -v soffice`;
-    return rslt.exitCode === 0
+    return rslt.exitCode === 0;
   } else {
     return false;
   }
