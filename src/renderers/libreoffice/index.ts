a7f004788a79d71c68301b17914fb1be47fd39f4
Jamie Blair <info@orangemug.co.uk>
2024-11-15 10:54:04 +0000

    More logging

--- src/renderers/libreoffice/index.ts
+++ src/renderers/libreoffice/index.ts
@@ -66,10 +66,12 @@ export async function render(
 }
 
 export async function isSupported(format: OfficeOpenXmlType) {
+  console.log("FORMATS.includes(format)=", FORMATS.includes(format))
   if (FORMATS.includes(format)) {
     return false;
   }
 
+  console.log("platform=", platform)
   if (platform === "win32") {
     const rslt =
       await $`where C:\\Program Files (x86)\\LibreOffice 5\\program\\soffice.exe`;
