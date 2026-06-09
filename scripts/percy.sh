6a6b0ca4165b7e635343931cad296841d1a4d786
Jamie Blair <info@orangemug.co.uk>
2026-06-09 20:02:00 +0100

    fix: pnpm build

--- scripts/percy.sh
+++ scripts/percy.sh
@@ -3,8 +3,8 @@ set -e
 
 ./node_modules/.bin/tsx ./bin/ooxml-render.ts render ./scripts/test-files/simple.docx
 
-npx percy snapshot ./docx-files
+pnpx percy snapshot ./docx-files
 # find ./docx-files -name '*.html' -print0 | 
 #     while IFS= read -r -d '' filepath; do 
-#         npx percy snapshot "$filepath"
+#         pnpx percy snapshot "$filepath"
 #     done
